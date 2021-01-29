65afd605c34bd6d10050aa4d8ca2bcfea6e5f2c7
carlosfelipeg <carlosfelipe20082008@hotmail.com>
2021-01-29 10:15:33 -0500

    menu navegation

--- js/menu.js
+++ js/menu.js
@@ -3,4 +3,10 @@ const menu = document.querySelector('.menu-nav');
 
 hamburguer.addEventListener('click', ()=>{
     menu.classList.toggle("spread");
-})
+});
+
+window.addEventListener('click', e=>{
+    if(menu.classList.contains('spread') && e.target != menu && e.target != hamburguer){
+        menu.classList.toggle("spread");
+    }
+});
